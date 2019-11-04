c7574968d7523ca9b0832e2086e6a36117b1455a
Lenny4 <alexandre.beaujour@hotmail.fr>
2019-11-04 15:17:46 +0100

    can create condition to bet

--- estimation.php
+++ estimation.php
@@ -93,15 +93,99 @@ Ouvre ta console : Ctrl + Alt + i
         };
     }
 
+    function initRunnersBet(runnersName) {
+        const runnerBets = [];
+        runnersName.map((name) => {
+            runnerBets.push({
+                runnerName: name,
+                bets: [],
+            });
+        });
+        return runnerBets;
+    }
+
+    function getBorne(runner, avgByRunner, time) {
+        const avg = avgByRunner.find(x => x.runnerName === runner.runnerName).avg;
+        return {
+            borneSup: (ratio * Math.sqrt(time)) + avg,
+            borneInf: -(ratio * Math.sqrt(time)) + avg,
+        };
+    }
+
+    function addMissingBet(runnerBets, lastObj) {
+        runnerBets.map((myRunnerBet) => {
+            if (myRunnerBet.bets.length % 2 !== 0) {
+                const lastBetType = myRunnerBet.bets[myRunnerBet.bets.length - 1].type;
+                const runner = lastObj.runners.find(x => x.runnerName === myRunnerBet.runnerName);
+                if (lastBetType === "back") {
+                    myRunnerBet.bets.push({
+                        name: "test",
+                        type: "lay",
+                        odd: runner.layOdd,
+                        time: 3600 + lastObj.time,
+                    });
+                } else if (lastBetType === "lay") {
+                    myRunnerBet.bets.push({
+                        name: "test",
+                        type: "back",
+                        odd: runner.backOdd,
+                        time: 3600 + lastObj.time,
+                    });
+                } else {
+                    alert("error !!!!");
+                }
+            }
+        });
+    }
+
+    function bet(runner, runnerBets, bornes, time) {
+        const myRunnerBet = runnerBets.find(x => x.runnerName === runner.runnerName);
+        let lastBetType = null;
+        if (myRunnerBet.bets.length > 0) lastBetType = myRunnerBet.bets[myRunnerBet.bets.length - 1].type;
+        if (lastBetType === null || lastBetType === "lay" || myRunnerBet.bets.length % 2 === 0) {
+            const type = "back";
+            if (runner.backOdd > bornes.borneSup) {
+                myRunnerBet.bets.push({
+                    name: "test",
+                    type: type,
+                    odd: runner.backOdd,
+                    time: 3600 + time,
+                });
+            }
+        } else if (lastBetType === null || lastBetType === "back" || myRunnerBet.bets.length % 2 === 0) {
+            const type = "lay";
+            if (runner.backOdd < bornes.borneInf) {
+                myRunnerBet.bets.push({
+                    name: "test",
+                    type: type,
+                    odd: runner.backOdd,
+                    time: 3600 + time,
+                });
+            }
+        }
+    }
+
     // c'est ici qu'on travaille
     $(document).ready(() => {
         matchs.map((match) => {
             const result = getFormatedMatchAndAvg(match);
             const runnersName = result.runnersName;
             const avgByRunner = result.avgByRunner;
-            console.log(runnersName);// les noms des runners qui nous intéresse
-            console.log(avgByRunner);// les moyenne des cotes de ces runners sur 2000s (avgBeforeTime)
-            console.log(match);// les données du match
+            const runnerBets = initRunnersBet(runnersName);
+            let lastObj = null;
+            match.json.map((obj, index) => {
+                if (3600 + obj.time > avgBeforeTime) {
+                    obj.runners.map((runner) => {
+                        if (runnersName.includes(runner.runnerName) && obj.volume > 0) {
+                            const bornes = getBorne(runner, avgByRunner, 3600 + obj.time);
+                            bet(runner, runnerBets, bornes, obj.time);
+                            lastObj = obj;
+                        }
+                    });
+                }
+            });
+            addMissingBet(runnerBets, lastObj);
+            console.log(runnerBets);
         });
     });
 </script>
