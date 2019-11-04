3d6b3630a50afb2a86acca620dab208ecd61c407
Lenny4 <alexandre.beaujour@hotmail.fr>
2019-11-04 14:18:44 +0100

    can start work on estimation

--- estimation.php
+++ estimation.php
@@ -38,9 +38,70 @@ Ouvre ta console : Ctrl + Alt + i
 </script>
 
 <script>
+    const maxRunnerOdd = 10;
+    const avgBeforeTime = 2000;
+
+    function getFormatedMatchAndAvg(match) {
+        const runnerNameToNotDisplay = [];
+        const runnersName = [];
+        const oddsByRunners = [];
+        // selectionne les runners à afficher
+        match.json.map((obj, index) => {
+            const runners = obj.runners.filter(x => x.backOdd > maxRunnerOdd);
+            if (Array.isArray(runners) && runners.length > 0) {
+                runners.map((runner) => {
+                    if (typeof runner === "object" && runner !== null && !runnerNameToNotDisplay.includes(runner.runnerName)) {
+                        runnerNameToNotDisplay.push(runner.runnerName);
+                    }
+                });
+            }
+        });
+        // initialise oddsByRunners
+        match.json[0].runners.map((runner, indexRunner) => {
+            if (runner.backOdd > 0 && !runnerNameToNotDisplay.includes(runner.runnerName)) {
+                runnersName.push(runner.runnerName);
+                oddsByRunners.push({
+                    runnerName: runner.runnerName,
+                    values: [],
+                    avg: null,
+                });
+            }
+        });
+        // récupère les moyennes des runners sur les avgBeforeTime premières secondes
+        match.json.map((obj, index) => {
+            if (obj.time > -3600 + avgBeforeTime && obj.volume > 0) {
+                obj.runners.map((runner, indexRunner) => {
+                    if (runnersName.includes(runner.runnerName)) {
+                        const oddRunner = oddsByRunners.find(x => x.runnerName === runner.runnerName);
+                        oddRunner.values.push(runner.backOdd);
+                    }
+                });
+            }
+        });
+        // calcule la moyenne pour tous les runners
+        oddsByRunners.map((runnerOdds) => {
+            runnerOdds.avg = runnerOdds.values.reduce(function (avg, value, _, {length}) {
+                return avg + value / length;
+            }, 0);
+        });
+        // retire la prop values de oddsByRunners
+        oddsByRunners.map((runnerOdds) => delete runnerOdds.values);
+        return {
+            runnersName: runnersName,
+            avgByRunner: oddsByRunners,
+        };
+    }
+
     // c'est ici qu'on travaille
     $(document).ready(() => {
-        console.log(matchs);
+        matchs.map((match) => {
+            const result = getFormatedMatchAndAvg(match);
+            const runnersName = result.runnersName;
+            const avgByRunner = result.avgByRunner;
+            console.log(runnersName);// les noms des runners qui nous intéresse
+            console.log(avgByRunner);// les moyenne des cotes de ces runners sur 2000s (avgBeforeTime)
+            console.log(match);// les données du match
+        });
     });
 </script>
 </body>
